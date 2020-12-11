f915c2b6350544372a8847d77c7c66112c006d52
Mihkail Glazatov <glazatov.mihail@gmail.com>
2020-12-11 11:06:23 +0700

    Testing auto-updates

--- ItemCreator/Properties/Settings.Designer.cs
+++ ItemCreator/Properties/Settings.Designer.cs
@@ -60,7 +60,7 @@ namespace ItemCreator.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0.0.1")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.0.2")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
